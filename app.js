c13d52d1b28de002b12d0286d65c71eaa77123d1
Santiago Castaño <santiagocastano589@gmail.com>
2024-03-03 17:25:32 -0500

    subo pagina de juegos multi idioma

---
+++ app.js
@@ -0,0 +1,90 @@
+function cambiarIdioma(idioma) {
+    if (idioma === 'es') {
+        document.getElementById('titulo').innerText = '🅿🅻🅰🆈🅴🆁🆂 🅿🅻🅰🅲🅴';
+
+        document.getElementById('fallGuysDescripcion').innerText = 'Fall Guys: Ultimate Knockout es un videojuego de plataformas multijugador desarrollado por Mediatonic y publicado por Devolver Digital. Fue lanzado en agosto de 2020 para PlayStation 4 y PC, y más tarde para otras plataformas como Nintendo Switch y Xbox. \n\nEl juego presenta un concepto único y divertido donde hasta 60 jugadores controlan simpáticos personajes llamados "Fall Guys" en una serie de competiciones estilo battle royale. El objetivo es superar obstáculos y completar desafíos de plataformas en una variedad de niveles temáticos, con el fin de ser el último en pie y ganar la corona.';
+        document.getElementById('fallGuysDescarga').innerText = 'Descargar';
+
+        document.getElementById('rocketLeagueDescripcion').innerText = 'Rocket League es un videojuego de deportes y conducción desarrollado y publicado por Psyonix. Fue lanzado en julio de 2015 para PlayStation 4, Xbox One y PC, y más tarde para Nintendo Switch. \n\nEl juego combina la emoción de los deportes de equipo con la adrenalina de la conducción de vehículos, colocando a los jugadores en partidos de fútbol (o soccer) donde controlan coches impulsados por cohetes en lugar de jugadores humanos. El objetivo es marcar goles golpeando una pelota gigante con los coches, utilizando una variedad de maniobras acrobáticas y habilidades para superar a los oponentes y defender la portería.';
+        document.getElementById('rocketLeagueDescarga').innerText = 'Descargar';
+
+        document.getElementById('valorantDescripcion').innerText = 'Valorant es un videojuego de disparos táctico en primera persona desarrollado y publicado por Riot Games. Fue lanzado en junio de 2020 y está disponible de forma gratuita para PC. \n\nEn Valorant, dos equipos de cinco jugadores se enfrentan en partidas competitivas donde el objetivo principal es plantar o desactivar una bomba llamada "Spike", similar al modo de juego de Counter-Strike. Sin embargo, Valorant incorpora elementos de juegos de disparos heroicos, ya que cada jugador elige un agente con habilidades únicas que pueden influir en el curso de la partida.';
+        document.getElementById('valorantDescarga').innerText = 'Descargar';
+
+        document.getElementById('nfsDescripcion').innerText = 'Need for Speed Heat es un videojuego de carreras desarrollado por Ghost Games y publicado por Electronic Arts. Fue lanzado en noviembre de 2019 para PlayStation 4, Xbox One y PC. \n\nEl juego se desarrolla en el vibrante mundo abierto de Palm City, una metrópolis ficticia inspirada en Miami, donde los jugadores participan en carreras callejeras ilegales mientras evitan a la policía. La historia sigue a un piloto novato que busca hacerse un nombre en la escena de las carreras callejeras y ganar fama y respeto entre los corredores.';
+        document.getElementById('nfsDescarga').innerText = 'Descargar';
+
+        document.getElementById('lolDescripcion').innerText = 'League of Legends (LoL) es un videojuego de estrategia en línea desarrollado y publicado por Riot Games. Fue lanzado en octubre de 2009 y ha ganado una inmensa popularidad desde entonces, convirtiéndose en uno de los juegos en línea más jugados del mundo. \n\nEn League of Legends, los jugadores controlan un personaje llamado "campeón", que tiene habilidades únicas y pertenece a una de las diferentes clases, como tanques, asesinos, magos, entre otros. El objetivo del juego es destruir el "Nexo" enemigo, una estructura ubicada en la base del equipo contrario, mientras se defiende la propia.';
+        document.getElementById('lolDescarga').innerText = 'Descargar';
+
+        document.getElementById('minecraftDescripcion').innerText = 'Minecraft es un videojuego de construcción y aventuras creado por Markus Persson y desarrollado por Mojang Studios. Fue lanzado inicialmente en 2011 para PC y posteriormente se ha expandido a numerosas plataformas, incluyendo consolas de videojuegos, dispositivos móviles y realidad virtual. \n\nEn Minecraft, los jugadores exploran un vasto mundo generado de manera procedimental, compuesto por bloques tridimensionales de diferentes materiales y biomas. Los jugadores pueden reunir recursos naturales, como madera, piedra y minerales, para crear herramientas, construir estructuras y enfrentarse a desafíos variados.';
+        document.getElementById('minecraftDescarga').innerText = 'Descargar';
+
+        document.getElementById('codDescripcion').innerText = 'Call of Duty: Modern Warfare 3 (MW3) es un videojuego de disparos en primera persona desarrollado por Infinity Ward y Sledgehammer Games, y publicado por Activision. Fue lanzado en noviembre de 2011 para varias plataformas, incluyendo PlayStation 3, Xbox 360, Wii y PC. \n\nModern Warfare 3 es la octava entrega de la serie Call of Duty y la tercera parte de la subserie Modern Warfare, continuando la historia de los conflictos modernos entre Estados Unidos y Rusia. El juego presenta una campaña para un jugador emocionante y cinematográfica, que sigue los eventos de su predecesor, Call of Duty: Modern Warfare 2, y concluye la trilogía Modern Warfare.';
+        document.getElementById('codDescarga').innerText = 'Descargar';
+
+        document.getElementById('mkDescripcion').innerText = 'Mortal Kombat 11 es un videojuego de lucha desarrollado por NetherRealm Studios y publicado por Warner Bros. Interactive Entertainment. Fue lanzado en abril de 2019 para PlayStation 4, Xbox One, Nintendo Switch y PC. \n\nEl juego es la undécima entrega principal de la serie Mortal Kombat y continúa la historia de los juegos anteriores, siguiendo los eventos de Mortal Kombat X. La trama gira en torno a la lucha entre varias facciones y personajes, incluyendo a los luchadores clásicos de la serie y a nuevos personajes introducidos en esta entrega.';
+        document.getElementById('mkDescarga').innerText = 'Descargar';
+
+
+        document.getElementById('haloDescripcion').innerText = 'Halo Reach es un videojuego de disparos en primera persona desarrollado por Bungie y publicado por Microsoft Game Studios. Fue lanzado en 2010 como precuela de la trilogía original de Halo y es la sexta entrega de la serie Halo. El juego está ambientado en el año 2552, durante los eventos que conducen al Halo: Combat Evolved original. \n\nEn Halo Reach, los jugadores asumen el papel de Noble Six, un miembro de un grupo de élite de soldados espartanos conocido como Noble Team. El juego tiene lugar en el planeta Reach, la mayor fortaleza militar de la humanidad y una ubicación estratégica clave en la guerra contra el Covenant alienígena.';
+        document.getElementById('haloDescarga').innerText = 'Descargar';
+
+
+        document.getElementById('mibDescripcion').innerText = 'MIB: Alien Crisis es un videojuego de acción en tercera persona basado en la popular franquicia cinematográfica "Men in Black". Fue desarrollado por Fun Labs y publicado por Activision. El juego fue lanzado en 2012 para varias plataformas, incluyendo Xbox 360, PlayStation 3 y Wii. \n\nEn MIB: Alien Crisis, los jugadores asumen el papel de Peter Delacoeur, un nuevo recluta en la agencia secreta Men in Black (Hombres de Negro). La trama del juego sigue a Peter mientras se enfrenta a una serie de misiones para detener una conspiración alienígena que amenaza con destruir la Tierra.';
+        document.getElementById('mibDescarga').innerText = 'Descargar';
+
+        document.getElementById('mgsDescripcion').innerText = 'Metal Gear Solid V: The Phantom Pain es un videojuego de acción y sigilo desarrollado por Kojima Productions y publicado por Konami. Fue lanzado en 2015 para varias plataformas, incluyendo PlayStation 3, PlayStation 4, Xbox 360, Xbox One y PC. \n\nEl juego es la quinta entrega principal de la serie Metal Gear Solid y sigue al legendario soldado Big Boss, también conocido como Venom Snake, en su misión de venganza contra aquellos que lo traicionaron y destruyeron su antigua organización militar privada, Militaires Sans Frontières (MSF).';
+        document.getElementById('mgsDescarga').innerText = 'Descargar';
+
+        document.getElementById('gtaDescripcion').innerText = 'Grand Theft Auto V (GTA V) es un videojuego de acción y aventura desarrollado por Rockstar North y publicado por Rockstar Games. Fue lanzado en septiembre de 2013 para PlayStation 3 y Xbox 360, y más tarde para PlayStation 4, Xbox One y PC. \n\nEl juego se desarrolla en el ficticio estado de San Andreas, una versión paródica de California, y sigue la historia de tres personajes principales: Michael De Santa, un ex ladrón de bancos que vive bajo protección de testigos; Franklin Clinton, un joven pandillero en busca de oportunidades; y Trevor Philips, un ex piloto de la Fuerza Aérea Canadiense convertido en traficante de drogas y psicópata.';
+        document.getElementById('gtaDescarga').innerText = 'Descargar';
+
+
+        document.getElementById('Descripcion').innerText = ''
+        document.getElementById('Descarga').innerText = ''
+
+        
+    } else if (idioma === 'en') {
+        document.getElementById('titulo').innerText = '🅿🅻🅰🆈🅴🆁🆂 🅿🅻🅰🅲🅴';
+
+        document.getElementById('fallGuysDescripcion').innerText = 'Fall Guys: Ultimate Knockout is a multiplayer platformer video game developed by Mediatonic and published by Devolver Digital. It was released in August 2020 for PlayStation 4 and PC, and later for other platforms such as Nintendo Switch and Xbox. \n\nThe game features a unique and fun concept where up to 60 players control cute characters called "Fall Guys" in a series of battle royale-style competitions. The goal is to overcome obstacles and complete platform challenges in a variety of themed levels, in order to be the last one standing and win the crown.';
+        document.getElementById('fallGuysDescarga').innerText = 'Download';
+
+        document.getElementById('rocketLeagueDescripcion').innerText = 'Rocket League is a sports and driving video game developed and published by Psyonix. It was released in July 2015 for PlayStation 4, Xbox One, and PC, and later for Nintendo Switch. \n\nThe game combines the excitement of team sports with the adrenaline of vehicle driving, placing players in soccer matches where they control rocket-powered cars instead of human players. The goal is to score goals by hitting a giant ball with the cars, using a variety of acrobatic maneuvers and skills to outplay opponents and defend the goal.';
+        document.getElementById('rocketLeagueDescarga').innerText = 'Download';
+
+        document.getElementById('valorantDescripcion').innerText = 'Valorant is a tactical first-person shooter video game developed and published by Riot Games. It was released in June 2020 and is available for free on PC. \n\nIn Valorant, two teams of five players compete in competitive matches where the main objective is to plant or defuse a bomb called the "Spike," similar to the gameplay of Counter-Strike. However, Valorant incorporates elements of hero shooter games, as each player chooses an agent with unique abilities that can influence the course of the match.';
+        document.getElementById('valorantDescarga').innerText = 'Download';
+
+        document.getElementById('nfsDescripcion').innerText = 'Need for Speed Heat is a racing video game developed by Ghost Games and published by Electronic Arts. It was released in November 2019 for PlayStation 4, Xbox One, and PC. \n\nThe game is set in the vibrant open world of Palm City, a fictional metropolis inspired by Miami, where players engage in illegal street racing while evading the police. The story follows a novice racer looking to make a name for themselves in the street racing scene and earn fame and respect among the racers.';
+        document.getElementById('nfsDescarga').innerText = 'Download';
+
+        document.getElementById('lolDescripcion').innerText = 'League of Legends (LoL) is an online strategy video game developed and published by Riot Games. It was released in October 2009 and has gained immense popularity since then, becoming one of the most played online games in the world. \n\nIn League of Legends, players control a character called a "champion," who has unique abilities and belongs to one of the different classes, such as tanks, assassins, mages, among others. The objective of the game is to destroy the enemy "Nexus," a structure located at the base of the opposing team, while defending your own.';
+        document.getElementById('lolDescarga').innerText = 'Download';
+
+
+        document.getElementById('minecraftDescripcion').innerText = 'Minecraft is a construction and adventure video game created by Markus Persson and developed by Mojang Studios. It was initially released in 2011 for PC and has since expanded to numerous platforms, including gaming consoles, mobile devices, and virtual reality. \n\nIn Minecraft, players explore a vast procedurally generated world composed of three-dimensional blocks of different materials and biomes. Players can gather natural resources such as wood, stone, and minerals to craft tools, build structures, and face various challenges.';
+        document.getElementById('minecraftDescarga').innerText = 'Download';
+
+        document.getElementById('codDescripcion').innerText = 'Call of Duty: Modern Warfare 3 (MW3) is a first-person shooter video game developed by Infinity Ward and Sledgehammer Games, and published by Activision. It was released in November 2011 for various platforms, including PlayStation 3, Xbox 360, Wii, and PC. \n\nModern Warfare 3 is the eighth installment in the Call of Duty series and the third installment in the Modern Warfare sub-series, continuing the story of modern conflicts between the United States and Russia. The game features an exciting and cinematic single-player campaign, following the events of its predecessor, Call of Duty: Modern Warfare 2, and concluding the Modern Warfare trilogy.';
+        document.getElementById('codDescarga').innerText = 'Download';
+
+        document.getElementById('mkDescripcion').innerText = 'Mortal Kombat 11 is a fighting video game developed by NetherRealm Studios and published by Warner Bros. Interactive Entertainment. It was released in April 2019 for PlayStation 4, Xbox One, Nintendo Switch, and PC. \n\nThe game is the eleventh main installment in the Mortal Kombat series and continues the story of previous games, following the events of Mortal Kombat X. The plot revolves around the fight between various factions and characters, including classic fighters from the series and new characters introduced in this installment.';
+        document.getElementById('mkDescarga').innerText = 'Download';
+
+        document.getElementById('haloDescripcion').innerText = 'Halo Reach is a first-person shooter video game developed by Bungie and published by Microsoft Game Studios. It was released in 2010 as a prequel to the original Halo trilogy and is the sixth installment in the Halo series. The game is set in the year 2552, during the events leading up to the original Halo: Combat Evolved. \n\nIn Halo Reach, players take on the role of Noble Six, a member of an elite group of Spartan soldiers known as Noble Team. The game takes place on the planet Reach, humanity\'s largest military stronghold and a key strategic location in the war against the alien Covenant.';
+        document.getElementById('haloDescarga').innerText = 'Download';
+
+        document.getElementById('mibDescripcion').innerText = 'MIB: Alien Crisis is a third-person action video game based on the popular "Men in Black" film franchise. It was developed by Fun Labs and published by Activision. The game was released in 2012 for various platforms, including Xbox 360, PlayStation 3, and Wii. \n\nIn MIB: Alien Crisis, players take on the role of Peter Delacoeur, a new recruit in the secret agency Men in Black. The game\'s plot follows Peter as he undertakes a series of missions to stop an alien conspiracy that threatens to destroy Earth.';
+        document.getElementById('mibDescarga').innerText = 'Download';
+
+        document.getElementById('mgsDescripcion').innerText = 'Metal Gear Solid V: The Phantom Pain is an action and stealth video game developed by Kojima Productions and published by Konami. It was released in 2015 for various platforms, including PlayStation 3, PlayStation 4, Xbox 360, Xbox One, and PC. \n\nThe game is the fifth main installment in the Metal Gear Solid series and follows the legendary soldier Big Boss, also known as Venom Snake, on his mission of vengeance against those who betrayed him and destroyed his former private military organization, Militaires Sans Frontières (MSF).';
+        document.getElementById('mgsDescarga').innerText = 'Download';
+
+        document.getElementById('gtaDescripcion').innerText = 'Grand Theft Auto V (GTA V) is an action-adventure video game developed by Rockstar North and published by Rockstar Games. It was released in September 2013 for PlayStation 3 and Xbox 360, and later for PlayStation 4, Xbox One, and PC. \n\nThe game is set within the fictional state of San Andreas, a parody of California, and follows the story of three main characters: Michael De Santa, a former bank robber living under witness protection; Franklin Clinton, a young gangster looking for opportunities; and Trevor Philips, a former Canadian Air Force pilot turned drug dealer and psychopath.';
+    document.getElementById('gtaDescarga').innerText = 'Download';
+
+    }
+}
+
